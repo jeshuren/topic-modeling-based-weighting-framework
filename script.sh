25467037eaaa50ab1bda99f2d73c4bb218e2a416
Sudarsun Santhiappan <mail@sudarsun.in>
2017-04-18 18:34:14 +0530

    Added comments.

--- script.sh
+++ script.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# This script is used for computing svm distances for every CV split.
+
+# use the base file name here.
 filename='letterj.csvtrain'
 csv=".csv"
 echo $filename
+
+# run across 4 Cross validation rounds
 for j in {1..4}
 do
+	# run through 5 folds of every cross validation round.
 	for i in {1..5};
 	do
 		echo $filename
@@ -12,6 +18,8 @@ do
 		out="$filename$j$i"
 		echo $in
 		echo $out
+		
+		# run the svm distance computation routine.
 		python svm_distance.py -t 0 -c 2 -o $out $in
 	done
 done
